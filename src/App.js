652183419b16e4596006570e10c52951e22df053
William Gomes <76756233+WilliamGofe@users.noreply.github.com>
2021-03-17 15:14:31 -0300

    Layout Static
    Layout da dos produtos - Tudo estatico

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import Produtos from './components/Produtos/Produtos'
+import ProdutosCard from './components/Produtos/ProdutosCard'
+const Centralizados = styled.div`
+  width: 1200px; 
+  height: 100vh;
+  margin:auto; 
+  `
 
-function App() {
+class App extends React.Component  {
+
+  render(){
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Centralizados>
+        <Produtos></Produtos>
+
+      <div className = "Produtos-index">
+        <ProdutosCard></ProdutosCard>
+
+     </div>
+
+      </Centralizados>
+  </div>
+ 
   );
+
+  }
 }
 
 export default App;
