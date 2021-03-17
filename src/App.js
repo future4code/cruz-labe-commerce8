7c7c3f3e7c5b74f28aa39d87b38aba0971ef20a5
lucasbarroscosta <lucasbarroscosta@gmail.com>
2021-03-17 16:39:54 -0300

    coloquei os inputs do filtro, bem como pegar seus value's

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components'
-import Produtos from './components/Produtos/Produtos'
-import ProdutosCard from './components/Produtos/ProdutosCard'
+import Produtos from './components/produtos/produtos'
+import ProdutosCard from './components/produtos/ProdutosCard'
+import Filtros from './components/Filtros'
+
 const Centralizados = styled.div`
   width: 1200px; 
   height: 100vh;
@@ -9,6 +11,25 @@ const Centralizados = styled.div`
   `
 
 class App extends React.Component  {
+  
+    state = {
+      filtroMin: 100,
+      filtroMax: 1000,
+      filtroNome: 'Produto',
+      carrinho: []
+    }
+  
+    onChangeMinFilter = (event) => {
+      this.setState({filtroMin: event.target.value})
+    }
+  
+    onChangeMaxFilter = (event) => {
+      this.setState({filtroMax: event.target.value})
+    }
+  
+    onChangeNameFilter = (event) => {
+      this.setState({filtroNome: event.target.value})
+    }
 
   render(){
   
@@ -24,6 +45,8 @@ class App extends React.Component  {
      </div>
 
       </Centralizados>
+
+      <Filtros></Filtros>
   </div>
  
   );
---
+++ src/components/Filtros.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FiltroContainer = styled.div`
+    
+    
+    background-color: #d3d3d3;
+    border: 1px solid black;
+    padding: 8px;
+
+`;
+
+const InputContainer = styled.label`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: 8px;
+`
+const Centralizados = styled.div`
+  width: 1200px; 
+  height: 100vh;
+  margin:auto;`
+
+
+export class Filtros extends React.Component {
+
+    render() {
+      return (
+          <div>
+            <Centralizados>
+        <FiltroContainer>      
+        <h3>Filtros</h3>
+          
+            <h4>Valor mínimo:</h4>
+            <InputContainer>
+            <input
+              type="number"
+              value={this.props.filtroMin}
+              onChange={this.props.onChangefiltroMin}
+            />
+            </InputContainer>
+            
+            <h4>Valor máximo:</h4>
+            <InputContainer>
+            <input
+              type="number"
+              value={this.props.filtroMax}
+              onChange={this.props.onChangefiltroMax}
+            />
+            </InputContainer>
+            <h4>Busca por nome:</h4>
+            <InputContainer>
+            <input
+              type="text"
+              value={this.props. filtroNome}
+              onChange={this.props.onChangefiltroNome}
+            />
+            </InputContainer>
+          </FiltroContainer>
+          </Centralizados>
+          </div>
+      )
+    }
+  }
+  
+  export default Filtros;
